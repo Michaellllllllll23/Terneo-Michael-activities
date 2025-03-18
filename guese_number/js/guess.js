b4cfa918868ee90c8725478a28d75ec553b86976
unknown <terneomichael@gmail.com>
2025-03-17 17:29:54 -1200

    sample message

--- guese_number/js/guess.js
+++ guese_number/js/guess.js
@@ -1,14 +1,12 @@
 function checkNumber(){
-    const guess = document.getElementById('guess').value;
-if (guess == random){
-    document.getElementById('message').innerHTML="Correct";
-}
-else{
-    document.getElementById('message').innerHTML="Wrong";
-}
-else if (guess < random) {
-    message.innerHTML = "higher";  
+    const guess= parseInt(document.getElementById('guess').value);
+    const message = document.getElementById('massage');
+if (guess === random){
+    message.innerHTML="Correct";
+}else if (guess < random){
+    message.innerHTML="higher";
 } else {
     message.innerHTML = "lower";
 }
 }
+
